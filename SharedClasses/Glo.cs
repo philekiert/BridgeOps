b9014d8c2d4e9b88d6345fd56234fed406d40b1e
Phil Ekiert <philekiert@live.co.uk>
2025-02-27 12:10:02 +0000

    Application version incremented

--- SharedClasses/Glo.cs
+++ SharedClasses/Glo.cs
@@ -5,9 +5,9 @@ using System.Xml.Linq;
 using System.Reflection;
 
 // Application version
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
-[assembly: AssemblyInformationalVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyInformationalVersion("1.1.2")]
 
 public static class Glo
 {
