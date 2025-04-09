4b724d0fba38672bc83cacf504483a08d5537652
Phil Ekiert <philekiert@live.co.uk>
2025-04-09 13:18:16 +0100

    Resolved bug with DatePicker not reverting to null on clearing text (solution not ideal)

--- BridgeOpsClient/App.xaml.cs
+++ BridgeOpsClient/App.xaml.cs
@@ -135,6 +135,16 @@ namespace BridgeOpsClient
             return false;
         }
 
+        private void DateTimeClearValueHandler(object o, EventArgs e)
+        {
+            if (o is TextBox txt && txt.Text == "")
+            {
+                DatePicker? dp = GetParentControl<DatePicker>(txt);
+                if (dp != null)
+                    dp.SelectedDate = null;
+            }
+        }
+
         public static bool RowClashConfirm(Window? owner)
         {
             try
@@ -2888,6 +2898,17 @@ namespace BridgeOpsClient
             if (parentObject == null)
                 return null;
             return parentObject as Window;
+        }
+        public static T? GetParentControl<T>(DependencyObject child) where T : DependencyObject
+        {
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+            while (parentObject != null && !(parentObject is Window))
+            {
+                parentObject = VisualTreeHelper.GetParent(parentObject);
+                if (parentObject is T t)
+                    return t;
+            }
+            return null;
         }
     }
 
